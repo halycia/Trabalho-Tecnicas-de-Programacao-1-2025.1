b0813d829298a78f0f97d92fe8972d956bed6bb8
Luc4Sant0z <137066050+Luc4Sant0z@users.noreply.github.com>
2025-06-10 23:14:16 -0300

    Delete sapiens directory

--- sapiens/src/Main.java
+++
@@ -1,7 +0,0 @@
-import views.LoginView;
-
-public class Main {
-    public static void main(String[] args) {
-        new LoginView();
-    }
-}
--- sapiens/src/models/Aluno.java
+++
@@ -1,9 +0,0 @@
-package models;
-
-
-
-public class Aluno extends Pessoa {
-    public Aluno(String nome, String email, String senha) {
-        super(nome, email, senha);
-    }
-}
